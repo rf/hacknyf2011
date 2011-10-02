f49b3b7c70a2b74a9f2b944d3a4a1c73a50e472b
Steven Lu <tacticalazn@gmail.com>
2011-10-02 01:47:08 -0400

    added in page-page animation

--- testingexpress/public/javascript/index.js
+++ testingexpress/public/javascript/index.js
@@ -1,14 +1,15 @@
 var step = 1;
 var etsy_prod = null;
 var hp_food = null;
+var hp_hotel = null;
 
 function request_etsy_prod()
 {
 	$.ajax({
-		url: '/etsy/50',
+		url: '/etsy/50/3',
 		async: 'true',
 		dataType: 'json',
-		timeout: 8000, 
+		timeout: 1000, 
 		success: function ( data )
 		{ 
 			etsy_prod = data;
@@ -22,12 +23,26 @@ function request_hp_food()
 		url: '/hp/New+York/food/1',
 		async: 'true',
 		dataType: 'json',
-		timeout: 8000, 
+		timeout: 1000, 
 		success: function ( data )
 		{ 
 			hp_food = data;
 		}
-	})
+	});
+}
+
+function request_hp_hotel()
+{
+	$.ajax({
+		url: '/hp/New+York/hotel/2',
+		async: 'true',
+		dataType: 'json',
+		timeout: 1000, 
+		success: function ( data )
+		{ 
+			hp_hotel = data;
+		}
+	});
 }
 
 function handle_steps()
@@ -63,19 +78,35 @@ $(document).ready(function() {
 	toggle_pagination();
 	handle_steps();
 	
+	$('#step1-page').show('slide', {direction:'right'}, 250);
+	
 	$('#next-button').click(function() {
 		if (step != 5)
 			step++;
+		else
+			return;
+			
+		$('#step'+(step-1)+'-page').hide('slide', {direction:'left'}, 250);
+		$('#step'+step+'-page').show('slide', {direction:'right'}, 250);
+
 		toggle_pagination();
-		handle_steps();
+		handle_steps();		
 	});
 	
 	$('#prev-button').click(function() {
 		if (step != 1)
 			step--;
+		else
+			return;
+			
+		$('#step'+(step+1)+'-page').hide('slide', {direction:'right'}, 250);
+		$('#step'+step+'-page').show('slide', {direction:'left'}, 250);
+		
 		toggle_pagination();
 		handle_steps();
 	});
 	
-	request_hp_food();	
+	request_hp_food();
+	request_etsy_prod();	
+	request_hp_hotel();
 });
