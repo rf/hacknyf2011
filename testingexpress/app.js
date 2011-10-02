8768bb00991838e425514b40ac196827006e253b
Steven Lu <tacticalazn@gmail.com>
2011-10-01 22:41:54 -0400

    Added in the initial styling of the app, + buttons.

--- testingexpress/app.js
+++ testingexpress/app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -7,15 +6,16 @@ var express = require('express');
 var app = module.exports = express.createServer();
 var posts = require('./models/posts').posts('localhost', 27017);
 
-// Configuration
-
+/**
+ * Server configuration.
+ */
 app.configure(function(){
    app.set('views', __dirname + '/views');
    app.set('view engine', 'jade');
    app.use(express.bodyParser());
    app.use(express.methodOverride());
    app.use(express.cookieParser());
-   app.use(express.session({ secret: 'your secret here' }));
+   app.use(express.session({ secret: 'ga5uP7AKuprethew' }));
    app.use(require('stylus').middleware({ src: __dirname + '/public' }));
    app.use(app.router);
    app.use(express.static(__dirname + '/public'));
@@ -29,45 +29,12 @@ app.configure('production', function(){
    app.use(express.errorHandler()); 
 });
 
-// Routes
-
+/**
+ * Routes
+ */
 app.get('/', function(req, res){
-   posts.findAll(function (error, data) {
-      if (error) {
-         throw error;
-      }
-      res.render('index', {
-         title    : 'blog',
-         articles : data
-      });
-   });
+   res.render('index', { title: "cheapchap" });
 });
 
-app.get('/blog/new', function (req, res) {
-   res.render('new.jade', { title : "New Post" });
-});
-
-app.post('/blog/new', function (req, res) {
-   posts.save ({
-      title    : req.param('title'),
-      body     : req.param('body')
-   }, function (error, data) {
-      if (error) {
-         throw error;
-      }
-      res.redirect('/');
-   });
-});
-
-app.get('/blog/:id', function (req, res) {
-   posts.findById (req.params.id, function (error, post) {
-      if (error) {
-         throw error;
-      } else {
-         res.render('post', { title: 'myblog', article: post });
-      }
-   });
-});
-
-app.listen(3000);
+app.listen(3001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
