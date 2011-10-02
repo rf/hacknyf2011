e308af1bb06fcab336643c31765ea1279a45497a
Steven Lu <tacticalazn@gmail.com>
2011-10-02 00:31:36 -0400

    New layout

--- testingexpress/public/javascript/index.js
+++ testingexpress/public/javascript/index.js
@@ -1,8 +1,20 @@
 var step = 1;
 
-$(document).ready(function() { 
-	toggle_pagination();
-});
+function etsy_data_success(data)
+{
+	alert(data);
+}
+
+function handle_steps()
+{
+		if (step-1 >= 1)
+			$('.step'+(step-1)).animate({color:'#999'}, 'fast');
+
+		$(".step" + step).animate({color:'#000'}, 'fast');
+
+		if (step+1 <= 5)
+			$('.step'+(step+1)).animate({color:'#999'}, 'fast');
+}
 
 function toggle_pagination()
 {
@@ -21,3 +33,34 @@ function toggle_pagination()
 		$('.next-button').fadeTo('normal', 1);
 	}
 }
+
+$(document).ready(function() { 
+	toggle_pagination();
+	handle_steps();
+	
+	$('.next-button').click(function() {
+		if (step != 5)
+			step++;
+		toggle_pagination();
+		handle_steps();
+	});
+	
+	$('.prev-button').click(function() {
+		if (step != 1)
+			step--;
+		toggle_pagination();
+		handle_steps();
+	});
+	
+	var etsy_data = $.ajax({
+		url: '/etsy/50',
+		async: 'true',
+		dataType: 'json',
+		timeout: 8000, 
+		success: function ( data )
+		{ 
+		//	alert (data.name); 
+		}
+	});
+	
+});
