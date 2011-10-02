afb4a3dd28518faa078acd711110d449ea03c57b
Steven Lu <tacticalazn@gmail.com>
2011-10-01 23:06:48 -0400

    beginning jquery framework

--- testingexpress/public/javascript/index.js
+++ testingexpress/public/javascript/index.js
@@ -1,3 +1,23 @@
-$(document).ready(function() { 
+var step = 1;
 
+$(document).ready(function() { 
+	toggle_pagination();
 });
+
+function toggle_pagination()
+{
+	if (step == 1)
+	{
+		$('.next-button').fadeTo('normal', 1);
+		$('.prev-button').fadeTo('normal', 0);
+	}
+	else if (step == 5)
+	{
+		$('.next-button').fadeTo('normal', 0);
+	}
+	else
+	{
+		$('.prev-button').fadeTo('normal', 1);
+		$('.next-button').fadeTo('normal', 1);
+	}
+}
