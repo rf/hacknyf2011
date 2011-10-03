d01905e1c443d270d214363c6e9137a216fbb132
Jarek Sedlacek <jarek@cs.rutgers.edu>
2011-10-03 11:20:21 -0400

    updated app to run on 80, updated hypublic to use new yelp acct

--- app.js
+++ app.js
@@ -166,5 +166,5 @@ app.get("/etsy/:max_price/:num_to_return", function (req,res){
    );
 });
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- models/hyperpublic.js
+++ models/hyperpublic.js
@@ -5,16 +5,20 @@ var _ = require('underscore');
 var yelp = require("yelp").createClient({
 //            consumer_key: "swwatQcoJz1db2Ec2hFXZQ",
             ///consumer_key: "U5XZjjllVhC4gt1rtEabrw",
-            consumer_key: "fIAc81be_pt__L3TK47mNw",
+            //consumer_key: "fIAc81be_pt__L3TK47mNw",
+              consumer_key: "n4XmKiacYboWPR6IxXJwFg",
 //            consumer_secret: "-XTXcYvgPh8anInpAYrSImmOTHs",
             //consumer_secret: "0cw0RHLsBi4nKfNfGsWS6vC9AfA",
-            consumer_secret: "kU4yrKwddUBHnNa0Q_1BcMB1w04",
+            //consumer_secret: "kU4yrKwddUBHnNa0Q_1BcMB1w04",
+            consumer_secret: "38dff9Oiuicr_wft1lV7s95Ykdk",
 //            token: "jLzcmltuso2zCO51O6ID1S8UcePSiX_S",
            //token: "AiysCogTNSwz1QK5aVdS1i15GB7toLmc",
-          token: "Y83jY74FM3JIA5xsol7HEn5vurZsGGqi",
+          //token: "Y83jY74FM3JIA5xsol7HEn5vurZsGGqi",
+          token: "q7LZwXFikJjjZmTYUqcDZSYRKurqh71F",
 //            token_secret: "LlDzta29cjw-fh-4fH0eMNe2jbI"
             //token_secret: "TI_ApsukBkhlV9AulFuYdlh9RaI"
-            token_secret: "wYWG8RHd03YZa1MvXsc0juSVxxk"
+            //token_secret: "wYWG8RHd03YZa1MvXsc0juSVxxk"
+            token_secret: "MSl_NfAtDISzUBaAuy1O3AFDqM8"
     });
 
 
